87b44de7dba6fd5e39ecdcdc8af771223d7e7229
Kamol <rusmirolub@gmail.com>
2021-04-07 09:57:42 +0100

    implemented main screen's logic. implemented createMovie UI.

--- app/src/main/java/com/example/kimovies/CreateMovie.java
+++ app/src/main/java/com/example/kimovies/CreateMovie.java
@@ -3,12 +3,22 @@ package com.example.kimovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateMovie extends AppCompatActivity {
+
+    private EditText titleText;
+    private EditText yearText;
+    private EditText directorText;
+    private EditText castText;
+    private EditText rateText;
+    private EditText reviewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_movie);
     }
+
+    private void setupEditText
 }
--- app/src/main/java/com/example/kimovies/MainActivity.java
+++ app/src/main/java/com/example/kimovies/MainActivity.java
@@ -2,13 +2,81 @@ package com.example.kimovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createMovieButton;
+    private Button displayMoviesButton;
+    private Button favouriteMoviesButton;
+    private Button editMoviesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setupButtons();
+        this.setupButtonListeners();
+    }
+
+    private void setupButtons() {
+        createMovieButton = (Button) findViewById(R.id.mainCreateMovieButton);
+        displayMoviesButton = (Button) findViewById(R.id.mainDisplayMoviesButton);
+        favouriteMoviesButton = (Button) findViewById(R.id.mainFavouriteMoviesButton);
+        editMoviesButton = (Button) findViewById(R.id.mainDisplayMoviesToEditButton);
+    }
+
+    private void setupButtonListeners() {
+        createMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateMovieActivity();
+            }
+        });
+
+        displayMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDisplayMoviesActivity();
+            }
+        });
+
+        favouriteMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFavouriteMoviesActivity();
+            }
+        });
+
+        editMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDisplayMoviesToEditActivity();
+            }
+        });
+    }
+
+    private void openCreateMovieActivity() {
+        Intent intent = new Intent(MainActivity.this, CreateMovie.class);
+        startActivity(intent);
+    }
+
+    private void openDisplayMoviesActivity() {
+        Intent intent = new Intent(MainActivity.this, DisplayMovies.class);
+        startActivity(intent);
+    }
+
+    private void openFavouriteMoviesActivity() {
+        Intent intent = new Intent(MainActivity.this, FavouriteMovies.class);
+        startActivity(intent);
+    }
+
+    private void openDisplayMoviesToEditActivity() {
+        Intent intent = new Intent(MainActivity.this, DisplayMoviesToEdit.class);
+        startActivity(intent);
     }
 }
