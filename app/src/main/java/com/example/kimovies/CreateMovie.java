a62ada84403b99b2157a56ef77e7f5d3db42e01c
Kamol <rusmirolub@gmail.com>
2021-04-07 10:04:22 +0100

    some fixes. added back button

--- app/src/main/java/com/example/kimovies/CreateMovie.java
+++ app/src/main/java/com/example/kimovies/CreateMovie.java
@@ -3,6 +3,8 @@ package com.example.kimovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class CreateMovie extends AppCompatActivity {
@@ -14,11 +16,44 @@ public class CreateMovie extends AppCompatActivity {
     private EditText rateText;
     private EditText reviewText;
 
+    private Button saveButton;
+    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_movie);
+
+        this.setupEditTexts();
     }
 
-    private void setupEditText
+    private void setupEditTexts() {
+        titleText = (EditText) findViewById(R.id.createMovieTitleText);
+        yearText = (EditText) findViewById(R.id.createMovieYearText);
+        directorText = (EditText) findViewById(R.id.createMovieDirectorText);
+        castText = (EditText) findViewById(R.id.createMovieCastText);
+        rateText = (EditText) findViewById(R.id.createMovieRateText);
+        reviewText = (EditText) findViewById(R.id.createMovieReviewText);
+    }
+
+    private  void setupButtons() {
+        saveButton = (Button) findViewById(R.id.createMovieSaveButton);
+        backButton = (Button) findViewById(R.id.createMovieBackButton);
+     }
+
+    private void setupButtonListeners() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
