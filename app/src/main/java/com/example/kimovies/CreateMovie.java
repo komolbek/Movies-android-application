72ad0467fae9a7f81dfb5fc3d2cb9e8396e73f54
Kamol <rusmirolub@gmail.com>
2021-04-07 09:24:54 +0100

    first commit

---
+++ app/src/main/java/com/example/kimovies/CreateMovie.java
@@ -0,0 +1,14 @@
+package com.example.kimovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_movie);
+    }
+}
---
+++ app/src/main/java/com/example/kimovies/EditMovie.java
@@ -0,0 +1,14 @@
+package com.example.kimovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_movie);
+    }
+}
